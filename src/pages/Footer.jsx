788f2b647447cb2f190c659de184c79e38643e4a
jjj000m <Jose.Santana1@walmart.com>
2023-11-22 22:05:56 -0600

    Ajuste al footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,43 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Footer(){
-    return(
-<footer className="text-center text-white" data-testid="footer">
-        {/* Grid container */}
-        <div className="container">
-            {/* Section: Social media */}
-            <section className="footer_media">
-                {/* Facebook */}
-                <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"><i
-                        className="fab fa-facebook-f"></i></Link>
+function Footer() {
+    return (
+        <footer className="text-center bg-dark" data-testid="footer">
+            {/* Grid container */}
+       
+            <div className="container">
+                <div className="row " id="socialMedia">
+                    <div className="col">
+                        {/* Facebook */}
+                        <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"><i
+                            className="fab fa-facebook-f"></i></Link>
 
-                {/* Twitter */}
-                <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                        className="fab fa-twitter"></i></Link>
+                        {/* Twitter */}
+                        <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            className="fab fa-twitter"></i></Link>
 
-                {/* Instagram */}
-                <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                        className="fab fa-instagram"></i></Link>
+                        {/* Instagram */}
+                        <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            className="fab fa-instagram"></i></Link>
 
-                {/* Youtube */}
-                <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                        className="fab fa-youtube"></i></Link>
-                
-                {/* Tiktok */}
-                <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                    className="fab fa-tiktok"></i></Link>
+                        {/* Youtube */}
+                        <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            className="fab fa-youtube"></i></Link>
 
-                   {/* Gmail */}
-                <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                    className="fab fa-google"></i></Link>   
+                        {/* Tiktok */}
+                        <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            className="fab fa-tiktok"></i></Link>
 
-                {/* What's app */}
-                <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                   className="fab fa-whatsapp"></i></Link>
-            </section>
-        </div>
-    </footer>
+                        {/* Gmail */}
+                        <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            className="fab fa-google"></i></Link>
+
+                        {/* What's app */}
+                        <Link className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            className="fab fa-whatsapp"></i></Link>
+                    </div>
+                </div>
+             
+                <div className="row" id="subFooter">
+                    <div className="col">
+                        <h5>Ragtime </h5><h6>Company</h6>
+                    </div>
+                </div>
+            </div>
+        </footer>
 
     )
 }
