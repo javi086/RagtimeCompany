93b874a5444cb5a90a7f2e17e61842ee701f4e17
jjj000m <Jose.Santana1@walmart.com>
2024-02-25 18:59:10 -0600

    CRUD completo en frontEnd

--- src/pages/EventsPlanner.jsx
+++ src/pages/EventsPlanner.jsx
@@ -5,6 +5,7 @@ import EventService from "../services/EventService";
 
 function EventsPlanner() {
 
+
   //POST
   const [eventInfo, setEventInfo] = useState({
     name: "",
@@ -12,134 +13,179 @@ function EventsPlanner() {
     startDate: "",
     endDate: ""
   });
-  
-  const saveEventInfo = (e) =>{
+
+  const addEvent = (e) => {
     e.preventDefault();
+    console.eventInfo;
     EventService.addEvent(eventInfo).then(response => {
       console.log(response.data);
-    }).catch(error =>{console.log(error)})
-    
+    }).catch(error => { console.log(error) })
   };
 
   //GET
   const [events, setEvents] = useState([]);
 
-  useEffect(() =>{
+  useEffect(() => {
+    let isMounted = true;
     EventService.getAllEvents().then(response => {
-      setEvents(response.data);
-      console.log(response.data);
+      if (isMounted) {
+        setEvents(response.data);
+        console.log(response.data);
+      }
     }).catch(error => {
       console.log(error);
-    })
-  },[])
+    });
+    return () => {
+      isMounted = false;
+    };
+  }, [])
 
-  //POST  
+  //PUT
 
+    const [editableRow, setEditableRow] = useState(null);
 
-  return (
-    <>
+    const editEventInfo = (eventId) => {
+      setEditableRow((prevEditableRow) => {
+        return prevEditableRow === eventId ? null : eventId;
+      });
+    };
 
-      <Container fluid as="div" id="eventPlanner">
-        <Row>
-          <Col>
-            <Accordion >
-              <Accordion.Item eventKey="0">
-                <Accordion.Header style={{ backgroundColor: 'lightblue', fontSize: '18px' }}>
-                  <span style={{ color: "red" }}>Agregador de eventos</span>
-                </Accordion.Header>
-                <Accordion.Body>
-                  <Form>
-                    <Row><Col><h4>Información inicial del evento</h4></Col></Row>
-                    <Row className="mb-3">
-                      <Form.Group as={Col} controlId="formName">
-                        <Form.Label>Nombre</Form.Label>
-                        <Form.Control 
-                        type="text" 
-                        placeholder="Nombre del evento" 
-                        name="name"
-                        value={eventInfo.name}
-                        onChange={(e) => setEventInfo({...eventInfo, name: e.target.value})}
-                        />
-                      </Form.Group>
+    const saveEventEdited = (eventId) => {
+      console.log(eventId);
+      console.log(eventInfo);
+      EventService.updateEventById(eventId, eventInfo).then(response => {
+        console.log(response.data);
+      }).catch(error => { console.log(error) })
+      console.log(`Save event with ID ${editableRow}`);
+      setEditableRow(null);
+    };
 
-                      <Form.Group as={Col} controlId="formLocation">
-                        <Form.Label>Ubicación</Form.Label>
-                        <Form.Control 
-                        type="text" 
-                        name='location'
-                        value={eventInfo.location}
-                        onChange={(e) => setEventInfo({...eventInfo, location: e.target.value})}
-                        />
-                      </Form.Group>
-                    </Row>
-                    <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formStartDate">
-                        <Form.Label>Fecha de Inicio</Form.Label>
-                        <Form.Control 
-                        type="date" 
-                        name="startDate"
-                        value={eventInfo.startDate}
-                        onChange={(e) => setEventInfo({...eventInfo, startDate: e.target.value})}
-                        />
-                      </Form.Group>
-                      <Form.Group as={Col} controlId="formEndDate">
-                        <Form.Label>Fecha de termino</Form.Label>
-                        <Form.Control 
-                        type="date" 
-                        name="endDate"
-                        value={eventInfo.endDate}
-                        onChange={(e) => setEventInfo({...eventInfo, endDate: e.target.value})}
-                        />
-                      </Form.Group>
-                    </Row>
-                    <Button variant="primary" type="submit" onClick={ (e) => saveEventInfo(e)}>
-                      Agregar
-                    </Button>
-                  </Form>
-                </Accordion.Body>
-              </Accordion.Item>
+    const deleteEvent = (eventId) =>{
+      EventService.deleteEventById(eventId).then((response) => {
+        console.log(response.data);
+      }).catch(error => { console.log(error)})
+      console.log(`Event deleted with ID ${editableRow}`);
+    }
 
+    return (
+      <>
 
-             { /*Seccion para mostrar los eventos creados*/}
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>Nombre del evento</Accordion.Header>
-                <Accordion.Body style={{ color: 'black' }}>
-                  <Table striped bordered hover>
-                    <thead>
-                      <tr>
-                        <th>Id</th>
-                        <th>Nombre</th>
-                        <th>Location</th>
-                        <th>Fecha Inicio</th>
-                        <th>Fecha Termino</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {
-                        events.map(
-                          event =>
-                        <tr key={event.id}>
-                        <td>{event.id}</td>
-                        <td>{event.name}</td>
-                        <td>{event.location}</td>
-                        <td>{event.startDate}</td>
-                        <td>{event.endDate}</td>
-                      </tr>
-                      )
-                      }
-                    </tbody>
-                  </Table>
+        <Container fluid as="div" id="eventPlanner">
+          <Row>
+            <Col>
+              <Accordion defaultActiveKey={['0']} alwaysOpen>
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header style={{ backgroundColor: 'lightblue', fontSize: '18px' }}>
+                    <span style={{ color: "red" }}>Agregador de eventos</span>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <Form>
+                      <Row><Col><h4>Información inicial del evento</h4></Col></Row>
+                      <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formName">
+                          <Form.Label>Nombre</Form.Label>
+                          <Form.Control
+                            type="text"
+                            placeholder="Nombre del evento"
+                            name="name"
+                            onChange={(e) => setEventInfo({ ...eventInfo, name: e.target.value })}
+                          />
+                        </Form.Group>
 
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-          </Col>
-        </Row>
+                        <Form.Group as={Col} controlId="formLocation">
+                          <Form.Label>Ubicación</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name='location'
+                            onChange={(e) => setEventInfo({ ...eventInfo, location: e.target.value })}
+                          />
+                        </Form.Group>
+                      </Row>
+                      <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formStartDate">
+                          <Form.Label>Fecha de Inicio</Form.Label>
+                          <Form.Control
+                            type="date"
+                            name="startDate"
+                            onChange={(e) => setEventInfo({ ...eventInfo, startDate: e.target.value })}
+                          />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="formEndDate">
+                          <Form.Label>Fecha de termino</Form.Label>
+                          <Form.Control
+                            type="date"
+                            name="endDate"
+                            onChange={(e) => setEventInfo({ ...eventInfo, endDate: e.target.value })}
+                          />
+                        </Form.Group>
+                      </Row>
+                      <Button variant="primary" type="submit" onClick={(e) => addEvent(e)}>
+                        Agregar
+                      </Button>
+                    </Form>
+                  </Accordion.Body>
+                </Accordion.Item>
 
 
-      </Container>
-    </>
-  )
+                { /*Seccion para mostrar los eventos creados*/}
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Eventos registrados</Accordion.Header>
+                  <Accordion.Body style={{ color: 'black' }}>
+                    <Table striped bordered hover>
+                      <thead>
+                        <tr>
+                          <th>Id</th>
+                          <th>Nombre</th>
+                          <th>Location</th>
+                          <th>Fecha Inicio</th>
+                          <th>Fecha Termino</th>
+                          <th>Acciones</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {events.map((event) => (
+                          <tr key={event.id}>
+                            <td>{event.id}</td>
+                            <td>{editableRow === event.id ? (
+                              <Form.Control type="text"   onChange={(e) => setEventInfo({ ...eventInfo, name: e.target.value })} />
+                            ) : (event.name)}
+                            </td>
+                            <td>{editableRow === event.id ? (
+                              <Form.Control type="text"  onChange={(e) => setEventInfo({ ...eventInfo, location: e.target.value })} />
+                            ) : (event.location)}
+                            </td>
+                            <td>{editableRow === event.id ? (
+                              <Form.Control type="date"  onChange={(e) => setEventInfo({ ...eventInfo, startDate: e.target.value })}/>
+                            ) : (event.startDate)}
+                            </td>
+                            <td>{editableRow === event.id ? (
+                              <Form.Control type="date"  onChange={(e) => setEventInfo({ ...eventInfo, endDate: e.target.value })}/>
+                            ) : (event.endDate)}
+                            </td>
+                            <td>
+                              {editableRow === event.id ? (
+                                <>
+                                  <Button variant="success" onClick={() => saveEventEdited(event.id)}>Guardar</Button>
+                                  <Button variant="secondary" onClick={() => editEventInfo(event.id)}>Cancelar</Button>
+                                </>
+                              ) : (
+                                <>
+                                <Button variant="primary" onClick={() => editEventInfo(event.id)}>Editar</Button>
+                                <Button variant="danger" onClick={() => deleteEvent(event.id)}>Eliminar</Button>
+                                </>
+                              )}
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Col>
+          </Row>
+        </Container>
+      </>)
 
-}
-export default EventsPlanner;
+  }
+  export default EventsPlanner;
--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,17 +1,29 @@
 import axios from "axios";
 
 
-const EVENT_BASE_REST_API_URL = "http://localhost:8080/api/v1/events/";
-const ADD_NEW_EVENT = "http://localhost:8080/api/v1/events/addEvent"
+const EVENT_BASE_REST_API_URL = "http://localhost:8080/api/v1/events";
 
 class EventService{
     getAllEvents(){
-        return axios.get(EVENT_BASE_REST_API_URL);
+        return axios.get(EVENT_BASE_REST_API_URL + '/');
     }
 
     addEvent(event){
-        return axios.post(ADD_NEW_EVENT, event);
+        return axios.post(EVENT_BASE_REST_API_URL + '/addEvent', event);
     }
 
+    getEventById(eventId){
+        return axios.get(EVENT_BASE_REST_API_URL + '/getEventById/' + eventId);
+    }
+
+    updateEventById(eventId, eventUpdated){
+        return axios.put(EVENT_BASE_REST_API_URL + '/updateEventById/'+ eventId, eventUpdated);
+    }
+    deleteEventById(eventId){
+        return axios.delete(EVENT_BASE_REST_API_URL + '/deleteEventById/' + eventId);
+    }
+
+
+
 }
-export default new EventService;
+export default new EventService();
