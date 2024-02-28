f7ea74bad156dfb138525a0535011657413bea83
jjj000m <Jose.Santana1@walmart.com>
2024-02-27 23:16:24 -0600

    Cambio en EventPlanner casi terminado

--- src/pages/EventsPlanner.jsx
+++ src/pages/EventsPlanner.jsx
@@ -6,32 +6,8 @@ import EventValidator from "../../scripts/EventValidator";
 
 
 function EventsPlanner() {
-  const prueba = () => {
-    const someValue = EventValidator.hola();
-    console.log(someValue);
-  }
-  const prueba2 = () => {
-    console.log("Hola");
-  }
 
-  //POST
-  const [eventInfo, setEventInfo] = useState({
-    name: "",
-    location: "",
-    startDate: "",
-    endDate: ""
-  });
-
-  const addEvent = (e) => {
-    e.preventDefault();
-    console.eventInfo;
-    EventService.addEvent(eventInfo).then(response => {
-      console.log(response.data);
-    }).catch(error => { console.log(error) })
-  };
-
-  //GET
-
+   //GET
   const [events, setEvents] = useState([]);
   useEffect(() => {
     EventService.getAllEvents().then(response => {
@@ -39,46 +15,51 @@ function EventsPlanner() {
     }).catch(error => { console.log(error) })
   }, [])
 
+  const [eventInfo, setEventInfo] = useState({
+    name: "",
+    location: "",
+    startDate: "",
+    endDate: ""
+  });
 
+//POST
+  const addEvent = (e) => {
+    e.preventDefault();
+    EventService.addEvent(eventInfo).then(response => {
+      console.log(response.data);
+    }).catch(error => { console.log(error) })
+    console.log("Event successfully added");
+  };
 
   //PUT
   const [isVisible, setVisibility] = useState(true);
   const [isDisabled, setDisabled] = useState(true);
-  /*const [editedEventInfo, setEditedEventInfo] = useState({
-    name: eventInfo.name,
-    location: eventInfo.location,
-    startDate: eventInfo.startDate,
-    endDate: eventInfo.endDate});*/
 
   const updateEventInfo = () => {
     setDisabled(false);
     setVisibility(false);
 
   }
+  const cancelEventEdition=() => {
+    setDisabled(true);
+    setVisibility(true);
+  }
 
-  const [editableRow, setEditableRow] = useState(null);
-
-  const editEventInfo = (eventId) => {
-    setEditableRow((prevEditableRow) => {
-      return prevEditableRow === eventId ? null : eventId;
-    });
-  };
 
   const saveEventEdited = (eventId) => {
-    console.log(eventId);
-    console.log(eventInfo);
+    setVisibility(true);
+    setDisabled(true);
     EventService.updateEventById(eventId, eventInfo).then(response => {
       console.log(response.data);
     }).catch(error => { console.log(error) })
-    console.log(`Save event with ID ${editableRow}`);
-    setEditableRow(null);
+    console.log("Event successfully updated with Id: " +eventId);
   };
 
   const deleteEvent = (eventId) => {
     EventService.deleteEventById(eventId).then((response) => {
       console.log(response.data);
     }).catch(error => { console.log(error) })
-    console.log(`Event deleted with ID ${editableRow}`);
+    console.log("Event successfully deleted with Id: " + eventId);
   }
 
   return (
@@ -107,7 +88,6 @@ function EventsPlanner() {
                           onChange={(e) => setEventInfo({ ...eventInfo, name: e.target.value })}
                         />
                       </Form.Group>
-
                       <Form.Group as={Col} controlId="formLocation">
                         <Form.Label>Ubicación</Form.Label>
                         <Form.Control
@@ -141,23 +121,22 @@ function EventsPlanner() {
                   </Form>
                 </Accordion.Body>
               </Accordion.Item>
-
-              {/**PRUEBA DE FRONT  2*/}
+              {/**Mostrar todos los eventos registrados*/}
               {events.map((event) => (
+
                 <Accordion.Item key={event.id} eventKey={event.id}>
                   <Accordion.Header><h6>{event.name}</h6></Accordion.Header>
                   <Accordion.Body>
                     <Form>
-                      <Row><Col><h4>Información del evento</h4></Col></Row>
+                      <Row><Col><h4>Información del evento</h4></Col><Col>ID: {event.id}</Col></Row>
                       <Row className="mb-3">
                         <Form.Group as={Col} controlId="formName">
-                          <Form.Label>Nombre</Form.Label>
-                          
+                          <Form.Label>Nombre</Form.Label>                      
                             <Form.Control
                               type="text"
                               placeholder={event.name}
                               name="registeredName"
-                              value={!isDisabled ? eventInfo.name : "event.name"}
+                              value={!isDisabled ? eventInfo.name : event.name}
                               disabled={isDisabled}
                               onChange={(e) => setEventInfo({...eventInfo, name: e.target.value })}
                             />
@@ -166,8 +145,9 @@ function EventsPlanner() {
                           <Form.Label>Ubicación</Form.Label>
                           <Form.Control
                             type="text"
-                            name={event.location}
-                            value={isDisabled ? eventInfo.location : event.location}
+                            placeholder={event.location}
+                            name="registeredLocation"
+                            value={!isDisabled ? eventInfo.location : event.location}
                             disabled={isDisabled}
                             onChange={(e) => setEventInfo({...eventInfo, location: e.target.value })}
                           />
@@ -177,27 +157,30 @@ function EventsPlanner() {
                         <Form.Group as={Col} controlId="formStartDate">
                           <Form.Label>Fecha de Inicio</Form.Label>
                           <Form.Control
-                            type="text"
+                            type={isDisabled ? "text" : "date"}
                             name="registeredStartDate"
-                            value={event.startDate}
-                            disabled
+                            value={!isDisabled ?  eventInfo.startDate : event.startDate  }
+                            disabled={isDisabled}
+                            onChange={(e) => setEventInfo({...eventInfo, startDate: e.target.value})}
                           />
                         </Form.Group>
                         <Form.Group as={Col} controlId="formEndDate">
                           <Form.Label>Fecha de termino</Form.Label>
                           <Form.Control
-                            type="text"
+                            type={isDisabled ? "text" : "date"}
                             name="registeredEndDate"
-                            value={event.endDate}
-                            disabled
+                            value={!isDisabled ? eventInfo.endDate : event.endDate }
+                            disabled={isDisabled}
+                            onChange={(e) => setEventInfo({...eventInfo, endDate: e.target.value})}
                           />
                         </Form.Group>
                       </Row>
                       <Row>
                         <Form.Group as={Col} controlId="buttonsForms" style={{ display: 'inline' }}>
                           <button type="button" className={isVisible ? "btn btn-warning" : "hideMe"} onClick={updateEventInfo}>Actualizar</button>
-                          <button type="button" className={!isVisible ? "btn btn-success" : "hideMe"}>Guardar</button>
-                          <button type="button" className={!isVisible ? "btn btn-danger" : "hideMe"}>Cancelar</button>
+                          <button type="button" className={isVisible ? "btn btn-danger" : "hideMe"} onClick={()=> deleteEvent(event.id)}>Eliminar</button>
+                          <button type="button" className={!isVisible ? "btn btn-success" : "hideMe"} onClick={() => saveEventEdited(event.id)}>Guardar</button>
+                          <button type="button" className={!isVisible ? "btn btn-secondary" : "hideMe"} onClick={cancelEventEdition}>Cancelar</button>
                         </Form.Group>
                       </Row>
 
@@ -205,61 +188,6 @@ function EventsPlanner() {
                   </Accordion.Body>
                 </Accordion.Item>
               ))}
-
-              { /*Seccion para mostrar los eventos creados*/}
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>Eventos registrados</Accordion.Header>
-                <Accordion.Body style={{ color: 'black' }}>
-                  <Table striped bordered hover>
-                    <thead>
-                      <tr>
-                        <th>Id</th>
-                        <th>Nombre</th>
-                        <th>Location</th>
-                        <th>Fecha Inicio</th>
-                        <th>Fecha Termino</th>
-                        <th>Acciones</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {events.map((event) => (
-                        <tr key={event.id}>
-                          <td>{event.id}</td>
-                          <td>{editableRow === event.id ? (
-                            <Form.Control type="text" onChange={(e) => setEventInfo({ ...eventInfo, name: e.target.value })} />
-                          ) : (event.name)}
-                          </td>
-                          <td>{editableRow === event.id ? (
-                            <Form.Control type="text" onChange={(e) => setEventInfo({ ...eventInfo, location: e.target.value })} />
-                          ) : (event.location)}
-                          </td>
-                          <td>{editableRow === event.id ? (
-                            <Form.Control type="date" onChange={(e) => setEventInfo({ ...eventInfo, startDate: e.target.value })} />
-                          ) : (event.startDate)}
-                          </td>
-                          <td>{editableRow === event.id ? (
-                            <Form.Control type="date" onChange={(e) => setEventInfo({ ...eventInfo, endDate: e.target.value })} />
-                          ) : (event.endDate)}
-                          </td>
-                          <td>
-                            {editableRow === event.id ? (
-                              <>
-                                <Button variant="success" onClick={() => saveEventEdited(event.id)}>Guardar</Button>
-                                <Button variant="secondary" onClick={() => editEventInfo(event.id)}>Cancelar</Button>
-                              </>
-                            ) : (
-                              <>
-                                <Button variant="primary" onClick={() => editEventInfo(event.id)}>Editar</Button>
-                                <Button variant="danger" onClick={() => deleteEvent(event.id)}>Eliminar</Button>
-                              </>
-                            )}
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </Table>
-                </Accordion.Body>
-              </Accordion.Item>
             </Accordion>
           </Col>
         </Row>
